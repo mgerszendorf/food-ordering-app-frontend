1e6182c2d7edab986d780c84af43d7285122114d
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-08-19 22:41:27 +0200

    Change file name

--- src/store/actions/actions.ts
+++
@@ -1,7 +0,0 @@
-export const TOGGLE_MENU = "TOGGLE_MENU";
-
-export type ActionTypes = { type: typeof TOGGLE_MENU };
-
-export const toggleMenu = (state: boolean): ActionTypes => ({
-  type: TOGGLE_MENU,
-});
